0901da496979fe5fa330a143ea19ad43f8e27e76
Danny van Bruggen <55701+matozoid@users.noreply.github.com>
2021-12-02 21:42:39 +0100

    A few upgrades to get things working again

--- client/src/extension.ts
+++ client/src/extension.ts
@@ -4,7 +4,7 @@ import { Executable, LanguageClient, LanguageClientOptions, StreamInfo } from 'v
 export function activate(context: ExtensionContext) {
 
     const serverOptions: Executable = {
-        command: "C:/PROGRA~1/Zulu/zulu-10/bin/java",
+        command: "java",
         args: ["-jar", "F:/work/TeachYourselfLsp4j/server/build/libs/TeachYourselfLsp4j-1.0-SNAPSHOT.jar"]
     }
 
